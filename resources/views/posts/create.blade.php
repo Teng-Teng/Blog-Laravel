91ddb69023ec930864e0bf8f2f3c6b6dd38e7825
jackottawa <tteng1992@gmail.com>
2018-07-08 10:52:22 -0400

    Form Request Data and CSRF

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,30 @@
 
 @section ('content')
     <div class="col-md-8 blog-main">
-        <h1>Create a Post</h1>
+        <h1>Publish a Post</h1>
+        <hr>
+
+        <form method="POST" action="/posts">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <label for="title">Title:</label>
+                <input type="text" name="title" class="form-control" id="title" aria-describedby="emailHelp" placeholder="Enter text">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="body">Body:</label>
+                <textarea name="body" id="body" class="form-control" placeholder="the body"></textarea>
+            </div>
+
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Publish</button>
+        </form>
     </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Task;
 
 Route::get('/', 'PostsController@index');
 Route::get('/posts/create', 'PostsController@create');
+Route::post('/posts', 'PostsController@store');
 
 
 Route::get('/postsAlbum', 'PostsAlbumController@index');
