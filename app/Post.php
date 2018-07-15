3f5cc40cdd6e7fde1548fbadd5d4682d358298d6
jackottawa <tteng1992@gmail.com>
2018-07-14 22:05:53 -0400

    Archives

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 //use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Post extends Model
 {
@@ -31,5 +32,16 @@ class Post extends Model
 //        $this->comments()->create(['body' => $body]);
 
         $this->comments()->create(compact('body'));
+    }
+
+    public function scopeFilter($query, $filters)
+    {
+        if (isset($filters['month']) && $month = $filters['month']) {
+            $query->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if (isset($filters['year']) && $year = $filters['year']) {
+            $query->whereYear('created_at', $year);
+        }
     }
 }
